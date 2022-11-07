a6136b5d917f73c9f56aa354d07a80eec961fedd
luyosamson <luyosamson@gmail.com>
2022-11-08 00:30:06 +0300

    commit tree 47a02ca236eb273ea700fb619df4303228e9ed0c

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+
+    attr_reader :knowledge
+
+    def initialize(knowledge)
+        @knowledge=[]
+
+
+    end
+
+    def self.learn(knowledge)
+
+        @knowledge << knowledge
+
+    end
 
 end
